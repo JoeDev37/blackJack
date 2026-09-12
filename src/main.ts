56e58ef6beb281952cce4f25d1fa69068aa1dd94
JoeDev37 <alonejoe367@gmail.com>
2026-09-12 12:38:03 +0300

    BlackJack(coding)

--- src/main.ts
+++ src/main.ts
@@ -3,49 +3,52 @@ import PromptSync from "prompt-sync";
 
 const prompt = PromptSync();
 
-// const name: string = prompt('what is your name? ');
+// const fund: string | number =Number( prompt('what is your fund? '));
 
-// console.log(`Hello, ${name}`);
-// 🂡 🂢 🂣 🂤 🂥 🂦 🂧 🂨 🂩 🂪 🂫 🂭 🂮
-// 🂱 🂲 🂳 🂴 🂵 🂶 🂷 🂸 🂹 🂺 🂻 🂽 🂾
-// 🃁 🃂 🃃 🃄 🃅 🃆 🃇 🃈 🃉 🃊 🃋 🃍 🃎
-// 🃑 🃒 🃓 🃔 🃕 🃖 🃗 🃘 🃙 🃚 🃛 🃝 🃞
-//
-//
-//
-// ♠A ♠2 ♠3 ♠4 ♠5 ♠6 ♠7 ♠8 ♠9 ♠10 ♠J ♠Q ♠K
-// ♥A ♥2 ♥3 ♥4 ♥5 ♥6 ♥7 ♥8 ♥9 ♥10 ♥J ♥Q ♥K
-// ♦A ♦2 ♦3 ♦4 ♦5 ♦6 ♦7 ♦8 ♦9 ♦10 ♦J ♦Q ♦K
-// ♣A ♣2 ♣3 ♣4 ♣5 ♣6 ♣7 ♣8 ♣9 ♣10 ♣J ♣Q ♣K
-//
-// 
+// if (!isNaN(fund)) {
+//   // console.log(`your bet $${bet}`)
 
-function shuffleArray(array: string[]): string[] {
-  for (let i = array.length - 1; i > 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1));
-    [array[i], array[randomIndex]] = [array[randomIndex], array[i]]
+//   if (fund <= 99) {
+//     console.log('fund too small(minimum $100)')
+//   } else if (fund>= 100) {
+//     console.log(`your fund $${fund}`)
+//   }
+// } else {
+//   console.log("put only number")
+// }
+
+const suits = ["♠", "♥", "♦", "♣"] as const;
+const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] as const;
+
+interface Card {
+  suit: typeof suits[number];
+  rank: typeof ranks[number];
+}
+
+
+function shuffledDeck<T>(deck: T[]): T[] {
+  for (let i = deck.length - 1; i > 0; i --) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
   }
-  return array;
+  return deck;
 }
 
-const suits = ["♠", "♥", "♦", "♣"];
-const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-const deck = [];
-
-for (let suit of suits) {
-  for (let rank of ranks) {
-    deck.push(suit + rank)
+const decks: Card[] = [];
+for (const suit of suits) {
+  for (const rank of ranks) {
+    decks.push({suit, rank})
+       // decks.pop()
   }
 }
 
-console.log(`Total Cards: ${deck.length}`);
-
-for (let i = 0; i < deck.length; i += 13) {
-  console.log(deck.slice(i, i + 13).join(" "));
+const shuffled = shuffledDeck(decks);
+// console.log(shuffled);
+const oneCard = shuffled.pop();
+if (oneCard) {
+  console.log(`${oneCard.rank}${oneCard.suit}`)
 }
 
-const topCard = deck.pop();
-console.log(`Top Card: ${topCard}`)
 
 
 
@@ -53,6 +56,91 @@ console.log(`Top Card: ${topCard}`)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function shuffleArray(array: string[]): string[] {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const randomIndex = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[randomIndex]] = [array[randomIndex], array[i]]
+//   }
+//   return array;
+// }
+
+// const suits: string[] = ["♠", "♥", "♦", "♣"];
+// const ranks: string[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+// const deck: any[] = [];
+
+// for (let suit of suits) {
+//   for (let rank of ranks) {
+//     deck.push(suit + rank)
+//   }
+// }
+
+// console.log(`Total Cards: ${deck.length}`);
+
+// for (let i = 0; i < deck.length; i += 13) {
+//   console.log(deck.slice(i, i + 13).join(" "));
+// }
+
+// const topCard = deck.pop();
+// console.log(`Top Card: ${topCard}`)
+
+// const topCard = Math.floor(Math.random () * deck.length)
+// console.log(`Top Card: ${topCard}`)
+
+
+// for (let suit of suits) {
+//   for (let rank of ranks) {
+//     const topCard = deck.pop();
+//     console.log(`top card: ${suit + rank}`)
+//     console.log( topCard)
+//   }
+// }
 
 
 
